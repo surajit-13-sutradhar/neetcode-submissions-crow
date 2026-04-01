131316033a0f5dbdf1b18851810941d3e237fa60
Surajit Sutradhar <surajit_ug_23@ee.nits.ac.in>
2026-04-02 00:05:46 +0530

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.py
@@ -0,0 +1,33 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        ROWS, COLS = len(board), len(board[0])
+
+        def dfs(r, c):
+            if r == ROWS or c == COLS or r < 0 or c < 0 or board[r][c] != 'O':
+                return
+
+            board[r][c] = "T" # the cell was visited
+            dfs(r - 1, c)
+            dfs(r + 1, c)
+            dfs(r, c - 1)
+            dfs(r, c + 1)
+
+        # Set the border cells to T temporary, as they are safe
+        for r in range(ROWS):
+            if board[r][0] == "O":
+                dfs(r, 0)
+            if board[r][COLS - 1] == "O":
+                dfs(r, COLS - 1)
+
+        for c in range(COLS):
+            if board[0][c] == "O":
+                dfs(0, c)
+            if board[ROWS - 1][c] == "O":
+                dfs(ROWS - 1, c)
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+                elif board[r][c] == "T":
+                    board[r][c] = "O"
